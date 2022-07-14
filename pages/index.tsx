c699b149e86f6ca490d7bd90e1384acad893df71
davidsiemers <davidsiemers94@gmail.com>
2022-07-14 08:12:44 -0400

    - Added SEO Metadata

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,35 @@
 import type { NextPage } from 'next'
+import { NextSeo } from 'next-seo'
+
 import Terminal from '../components/common/Terminal'
 
 const Home: NextPage = () => {
-  return <Terminal />
+  const metaDescription = `David Siemers is a full-stack software engineer with a
+      passion for creating awesome user-experiences`
+  const metaTitle = 'David Siemers | Home'
+
+  return (
+    <>
+      <NextSeo
+        title={metaTitle}
+        description={metaDescription}
+        openGraph={{
+          url: 'https://davidsiemers.com',
+          title: metaTitle,
+          description: metaDescription,
+          images: [
+            {
+              url: '/images/profile.jpg',
+              width: 512,
+              height: 512,
+              alt: 'David Siemers',
+            },
+          ],
+        }}
+      />
+      <Terminal />
+    </>
+  )
 }
 
 export default Home
